ab902995fbcf9bce6e635aee67447e36327cad58
Iippo Leppänen <21137181@edu.riveria.fi>
2021-04-28 15:03:49 +0300

    kuva video katsottu ai ei ole svg

--- src/Json.js
+++ src/Json.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import data from "./data.json";
 
+const url = "https://wiki.guildwars2.com/wiki/";
 
 class Json extends Component {
     render() {
@@ -9,18 +10,20 @@ class Json extends Component {
                 <h2>JSON datan esittäminen sivulla</h2>
                 {
                     data.Pets.map((pet, i) => {
+                        let petAddr2 = pet.name.replace(/\s/g, '_');
                         return (
                             <div key={i}>
                                 <div className="card">
                                     <h1>{pet.name}</h1>
                                     <img src={pet.icon} alt={pet.name} style={{ width: '60%' }}></img>
                                     <p className="price">{pet.description} </p>
+                                    <p><button type="submit"> <a href={url + petAddr2} target="_blank" rel="noreferrer" />Go to Wiki</button> </p>
                                 </div>
                             </div>
                         );
                     })
                 }
-            </div>
+            </div >
         );
     }
 }
--- src/Koti.js
+++ src/Koti.js
@@ -1,11 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import githubJpg from './resources/images/github.jpg'
+import githubPng from './resources/images/github.png'
+import githubSvg from './resources/images/github.ai'
+
 
 class Koti extends Component {
     render() {
-        return(
+        return (
             <div>
                 <h2>Moikka</h2>
-                <p>Tähän tekstiä. Tähän tekstiä. Tähän tekstiä..</p>
+                <p>Jpg kuva public kansiosta</p>
+                <img class="sample-icon" src="/images/github.jpg" alt="github kissa" />
+                <p>Jpg kuva importtattuna</p>
+                <img class="sample-icon" src={githubJpg} alt="github kissa" />
+                <p>PNG kuva importtattuna</p>
+                <img class="sample-icon" src={githubPng} alt="github kissa" />
+                <p>SVG kuva importtattuna</p>
+                <img class="sample-icon" src={githubSvg} alt="github kissa" />
+
             </div>
         );
     }
