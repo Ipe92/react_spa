c788a7813a814249d3252535039a541433796326
Iippo Leppänen <21137181@edu.riveria.fi>
2021-05-04 13:08:37 +0300

    listan tarinat json serveri 49min tarina dispatch

--- src/containers/Tarinat/Tarinat.js
+++ src/containers/Tarinat/Tarinat.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 import AddTarina from './containers/AddTarina';
 import TarinaLista from '../Tarinat/containers/TarinaLista';
+import * as tarinaActions from '../Tarinat/tarinaActions';
+
 
 class Tarinat extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Tarina</h2>
-                <p>Tähämpä sitten voi säveltää omaa juttuaan ja sivun sisältöä:</p>
-                <AddTarina />
-                <TarinaLista />
-            </div >
-        );
-    }
+	componentDidMount() {
+		fetch("http://localhost:4000/tarinat")
+			.then(response => response.json())
+			.then(data => {
+				data.forEach((tarina) => this.props.lisaaTarina(tarina));
+			});
+	}
+	render() {
+		return (
+			<div>
+				<h2>Tarina</h2>
+				<p>Tähämpä sitten voi säveltää omaa juttuaan ja sivun sisältöä:</p>
+				<AddTarina />
+				<TarinaLista />
+			</div >
+		);
+	}
 }
 
-export default Tarinat;
+export default connect(null, (dispatch) => bindActionCreators(tarinaActions, dispatch))(Tarinat);
