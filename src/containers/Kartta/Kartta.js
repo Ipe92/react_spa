a6f28078bf7bba5cf31d2aa8fd39ef502a1882b6
Ipe92 <Ipe92@users.noreply.github.com>
2021-05-17 18:17:02 +0300

    kartta marker tehty

--- src/containers/Kartta/Kartta.js
+++ src/containers/Kartta/Kartta.js
@@ -4,20 +4,26 @@ import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 //import "leaflet/dist/leaflet.css";
 
 const position = [62.600818, 29.762092];
-const isoMyyPosition = [62.601934, 29.763317];
+
+const fakeData = [
+  { name: "Tori", geoJson: { lat: 62.600818, lon: 29.762092 } },
+  { name: "Iso-Myy", geoJson: { lat: 62.601934, lon: 29.763317 } },
+];
 
 function Kartta(props) {
-    return (
-        <Map style={{ height: "400px" }} center={position} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={position}>
-                <Popup>Tori</Popup>
-            </Marker>
-            <Marker position={isoMyyPosition}>
-                <Popup>Iso-Myy</Popup>
-            </Marker>
-        </Map>
-    );
+  return (
+    <Map style={{ height: "400px" }} center={position} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {fakeData.map((location) => (
+        < Marker position={[location.geoJson.lat, location.geoJson.lon]} >
+          <Popup>{location.name}</Popup>
+        </Marker>
+      ))}
+    </Map >
+  );
 }
 
 export default Kartta;
