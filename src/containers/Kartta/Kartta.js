03538e50132741a81b336d365f76932f6dcdf028
Ipe92 <Ipe92@users.noreply.github.com>
2021-06-02 16:20:14 +0300

    bussi data ei toimi koska "busStops is undefined"

--- src/containers/Kartta/Kartta.js
+++ src/containers/Kartta/Kartta.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { icon as leafletIcon } from "leaflet";
@@ -7,68 +7,80 @@ import { icon as leafletIcon } from "leaflet";
 import gitHub from "../../resources/images/github.jpg";
 const position = [62.600818, 29.762092];
 const fakeData = [
-  {
-    name: "Tori",
-    geoJson: { lat: 62.600818, lng: 29.762092 },
-    image: gitHub
-  },
-  {
-    name: "Iso-Myy",
-    geoJson: { lat: 62.601934, lng: 29.763317 },
-    image: gitHub
-  },
+    {
+        name: "Tori",
+        geoJson: { lat: 62.600818, lng: 29.762092 },
+        image: gitHub,
+    },
+    {
+        name: "Iso-Myy",
+        geoJson: { lat: 62.601934, lng: 29.763317 },
+        image: gitHub,
+    },
 ];
 
 function Kartta(props) {
-  const [markersData, setMarkersData] = useState(fakeData);
+    const [markersData, setMarkersData] = useState(fakeData);
+    // const [busStops, setBusStops] = useState([]);
 
-  const onClick = (event) => {
-    setMarkersData([
-      ...markersData,
-      {
-        name: "Uusi",
-        geoJson: {
-          lat: event.latlng.lat,
-          lng: event.latlng.lng,
-        },
-        image: gitHub,
-      },
-    ]);
-  };
-  const customMarkerIcon = leafletIcon({
-    iconUrl: gitHub,
-    iconSize: [16, 16],
-    iconAnchor: [12, 12],
-    popupAnchor: [-3, -36],
-  });
-  return (
-    <Map
-      style={{ height: "400px" }}
-      center={position}
-      zoom={13}
-      onClick={(event) => onClick(event)}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="http://tile.thunderforest.com/landscape/{z}/{x}/{y}.png"
-      />
-      <TileLayer
-        url="https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png"
-      />
-      {markersData.map((location) => (
-        <Marker
-          key={JSON.stringify(location.geoJson)}
-          position={[location.geoJson.lat, location.geoJson.lng]}
-          icon={customMarkerIcon}
-        >
-          <Popup>
-            <img src={location.image} alt="paikka" />
-            {location.name}
-          </Popup>
-        </Marker>
-      ))}
-    </Map>
-  );
+    // useEffect(() => {
+    //     fetch("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-type": "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //             query: "{\n stops {\n gtfsId\n name\n lat\n lon\n zoneId\n }\n",
+    //             variables: null,
+    //         }),
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => setBusStops(data.data.stops));
+    // }, []);
+
+    const onClick = (event) => {
+        setMarkersData([
+            ...markersData,
+            {
+                name: "Uusi",
+                geoJson: {
+                    lat: event.latlng.lat,
+                    lng: event.latlng.lng,
+                },
+                image: gitHub,
+            },
+        ]);
+    };
+    const customMarkerIcon = leafletIcon({
+        iconUrl: gitHub,
+        iconSize: [16, 16],
+        iconAnchor: [12, 12],
+        popupAnchor: [-3, -36],
+    });
+    return (
+        <Map style={{ height: "400px" }} center={position} zoom={13} onClick={(event) => onClick(event)}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="http://tile.thunderforest.com/landscape/{z}/{x}/{y}.png"
+            />
+            <TileLayer url="https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png" />
+            {markersData.map((location) => (
+                <Marker
+                    key={JSON.stringify(location.geoJson)}
+                    position={[location.geoJson.lat, location.geoJson.lng]}
+                    icon={customMarkerIcon}
+                >
+                    <Popup>
+                        <img src={location.image} alt="paikka" />
+                        {location.name}
+                    </Popup>
+                </Marker>
+            ))}
+            {/* {busStops.slice(0, 100).map((busStop) => (
+                <Marker key={busStops.gtfsId} position={[busStop.lat, busStop.lon]} />
+            ))} */}
+        </Map>
+    );
 }
 
 export default Kartta;
